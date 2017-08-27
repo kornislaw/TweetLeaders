464dfe9315c0d8b74ba3d76034a0b0ae61f091a0
Dominik Piekarczyk <piekarczyk@gmail.com>
2017-08-27 21:17:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Source
+/Users/dominik/Documents/dev/tutorial-python-neo4j/
+
+https://www.youtube.com/watch?v=r0s7qr56xG4
+http://nicolewhite.github.io/neo4j-jupyter/main.html
+https://www.youtube.com/watch?v=jX9PBbPUoRY
+
+
+## Related links
+
+http://visjs.org/
+https://github.com/nicolewhite/neo4j-jupyter
+
+http://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
+
---
+++ tweetlead.py
@@ -0,0 +1,34 @@
+import os
+import tweepy
+from pprint import pprint as pp
+from tweepy import OAuthHandler
+
+
+# getting keys from OS ENV variables (stored there to avoid commiting secrets to the repo)
+consumer_key = os.environ['TWITTER_TWEETLEAD_CONSUMER_KEY']
+consumer_secret = os.environ['TWITTER_TWEETLEAD_CONSUMER_SECRET']
+access_token = os.environ['TWITTER_TWEETLEAD_ACCESS_TOKEN']
+access_secret = os.environ['TWITTER_TWEETLEAD_ACCESS_SECRET']
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+u_name = 'kornislaw'
+u = api.get_user(u_name)
+
+nodes = 'nodes = [\n'
+nodes += f"\t\t{{id: {u.id}, shape: 'circularImage', image: '{u.profile_image_url}', label:'{u.screen_name}'}},\n"
+for flr in api.followers(u_name):
+    nodes += f"\t\t{{id: {flr.id}, shape: 'circularImage', image: '{flr.profile_image_url}', label:'{flr.screen_name}'}},\n"
+nodes += '];\n'
+
+edges = 'edges = [\n'
+for flr in api.followers(u_name):
+    edges += f'\t\t{{from: {flr.id}, to: {u.id}}},\n'
+edges += '];\n'
+
+
+print(nodes)
+print(edges)
