77c43a092fbb6ac7484c3fbfcd3fefe24485d1ad
Dominik Piekarczyk <piekarczyk@gmail.com>
2017-08-30 20:30:03 +0200

    code refactored

--- tweetlead.py
+++ tweetlead.py
@@ -15,20 +15,35 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+# api.search_users('@AndrzejDuda')
 u_name = 'kornislaw'
-u = api.get_user(u_name)
 
-nodes = 'nodes = [\n'
-nodes += f"\t\t{{id: {u.id}, shape: 'circularImage', image: '{u.profile_image_url}', label:'{u.screen_name}'}},\n"
-for flr in api.followers(u_name):
-    nodes += f"\t\t{{id: {flr.id}, shape: 'circularImage', image: '{flr.profile_image_url}', label:'{flr.screen_name}'}},\n"
-nodes += '];\n'
+def print_followers():
+    u = api.get_user(u_name)
 
-edges = 'edges = [\n'
-for flr in api.followers(u_name):
-    edges += f'\t\t{{from: {flr.id}, to: {u.id}}},\n'
-edges += '];\n'
+    nodes = 'nodes = [\n'
+    nodes += f"\t\t{{id: {u.id}, shape: 'circularImage', image: '{u.profile_image_url}', label:'{u.screen_name}'}},\n"
+    for flr in api.followers(u_name):
+        nodes += f"\t\t{{id: {flr.id}, shape: 'circularImage', image: '{flr.profile_image_url}', label:'{flr.screen_name}'}},\n"
+    nodes += '];\n'
 
+    edges = 'edges = [\n'
+    for flr in api.followers(u_name):
+        edges += f'\t\t{{from: {flr.id}, to: {u.id}}},\n'
+    edges += '];\n'
 
-print(nodes)
-print(edges)
+    print(nodes)
+    print(edges)
+
+def get_followers(u_name):
+    u = api.get_user(u_name)
+    flrs = api.followers(u_name)
+    nodes = [{'id': flr.id, 'shape': 'circularImage', 'image': flr.profile_image_url, 'label': flr.screen_name} for flr in flrs]
+    nodes.append({'id': u.id, 'shape': 'circularImage', 'image': u.profile_image_url, 'label': u.screen_name, 'size': 70 })
+    edges = [{'from': flr.id, 'to': u.id} for flr in flrs]
+
+    return {'nodes': nodes, 'edges': edges}
+
+pp(get_followers('kornislaw'))
+# def print_followed():
+    # API.friends_ids(id/screen_name/user_id[, cursor]
